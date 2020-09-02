56052ef8a3cc29c50fa8e8a92cd5d2ed91dddac9
mehradsadeghi <2012.linkinpark@gmail.com>
2020-09-02 21:09:38 +0430

    Minor refactoring.

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -2,7 +2,6 @@
 
 namespace Imanghafoori\EloquentHistory\Tests;
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Imanghafoori\EloquentHistory\HistoryTracker;
 use Imanghafoori\EloquentHistory\Tests\Stubs\Models\DataChange;
@@ -11,15 +10,21 @@ use Imanghafoori\EloquentHistory\Tests\Stubs\Models\User;
 
 class IntegrationTest extends TestCase
 {
-    /** @test */
-    public function tracks_created_model_event()
+
+    protected function setUp(): void
     {
+        parent::setUp();
+
         Route::get('/', function() {});
         $this->get('/');
 
-        HistoryTracker::track(User::class);
+        $this->trackUser();
+    }
 
-        factory(User::class)->create();
+    /** @test */
+    public function tracks_created_model_event()
+    {
+        $this->createNewUser();
 
         $this->assertEquals(1, DataChangesMeta::count());
         $this->assertEquals(7, DataChange::count());
@@ -28,12 +33,7 @@ class IntegrationTest extends TestCase
     /** @test */
     public function tracks_updated_model_event()
     {
-        Route::get('/', function() {});
-        $this->get('/');
-
-        HistoryTracker::track(User::class);
-
-        $user = factory(User::class)->create();
+        $user = $this->createNewUser();
         $user->age = 10;
         $user->save();
 
@@ -51,12 +51,7 @@ class IntegrationTest extends TestCase
     /** @test */
     public function doesnt_track_updated_model_event_when_updating_bulkly()
     {
-        Route::get('/', function() {});
-        $this->get('/');
-
-        HistoryTracker::track(User::class);
-
-        $user = factory(User::class)->create();
+        $user = $this->createNewUser();
 
         User::whereId($user->id)->update(['age' => 10]);
 
@@ -67,23 +62,28 @@ class IntegrationTest extends TestCase
     /** @test */
     public function tracks_deleted_model_event()
     {
-        Route::get('/', function() {});
-        $this->get('/');
-
-        HistoryTracker::track(User::class);
-
-        $user = factory(User::class)->create();
+        $user = $this->createNewUser();
 
         User::destroy($user->id);
 
         $this->assertEquals(2, DataChangesMeta::count());
         $this->assertEquals(14, DataChange::count());
 
-        $user2 = factory(User::class)->create();
+        $user2 = $this->createNewUser();
 
         $user2->delete();
 
         $this->assertEquals(4, DataChangesMeta::count());
         $this->assertEquals(28, DataChange::count());
+    }
+
+    private function trackUser()
+    {
+        HistoryTracker::track(User::class);
+    }
+
+    private function createNewUser()
+    {
+        return factory(User::class)->create();
     }
 }
